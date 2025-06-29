e3d406d50af13acaa4fad0fbc06f0356edc14ec3
prafull shinde <prafullshinde7007@gmail.com>
2025-06-29 20:52:09 +0530

    cookie-parser change

--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 
+import {app} from './app.js'
 dotenv.config({
-    path : './env'
+    path: './.env'
 })
+
 
 
 connectDB()
-.then(()=>{
-    application.listen(process.env.PORT || 8000 ,()=>{
-        console.log(`server is runnning at port  :${process.env.PORT}`);
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((err)=>{
-console.log("MONGODB connection failed !!!");
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
-
 
 
 /*
