cbbdac2cfd4b460a2180b22c9d55b4297d252e73
prafull shinde <prafullshinde7007@gmail.com>
2025-06-29 18:34:50 +0530

    Clodinary And Multer

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUINARY_API_KEY, 
+        api_secret:process.env.CLOUINARY_API_SECRET 
+    });
+          
+    const uploadOnCloudinary = async (localFilePath)=>{
+        try{
+          if(!localFilePath) return null
+          //upload 
+         const response =await cloudinary.uploader.upload(localFilePath,{
+            resource_type : "auto"
+          })
+          // file has been uploaded successfully
+          console.log("file is uploaded on cloudinary",
+            response.url);
+            return response
+        }catch(error){
+            fs.unlinkSync(localFilePath) 
+            // remove the locally saved file as the upload operation got failed
+            return null
+        }
+    }
+
+      
+     return {uploadOnCloudinary}
